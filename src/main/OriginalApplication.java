a17ebfb6ce2b7e0824790ae25d703e2a0972008f
Plamen Milenkov <plamen.milenkov@sap.com>
2015-12-22 14:55:10 +0200

    Refactor HelloLucene application

--- src/main/OriginalApplication.java
+++ src/main/OriginalApplication.java
@@ -21,56 +21,64 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
 public class OriginalApplication {
+	private static StandardAnalyzer analyzerInstance;
+	private static Directory indexInstance;
+	
 	public static void main(String[] args) throws IOException, ParseException {
-        // 0. Specify the analyzer for tokenizing text.
-        //    The same analyzer should be used for indexing and searching
-        StandardAnalyzer analyzer = new StandardAnalyzer();
+        addDocuments();
+        
+        String queryString = args.length > 0 ? args[0] : "lucene";
+        Query query = new QueryParser("title", getAnalyzer()).parse(queryString);
 
-        // 1. create the index
-        Directory index = new RAMDirectory();
-
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-        IndexWriter writer = new IndexWriter(index, config);
-        addDoc(writer, "Lucene in Action", "193398817");
-        addDoc(writer, "Lucene for Dummies", "55320055Z");
-        addDoc(writer, "Managing Gigabytes", "55063554A");
-        addDoc(writer, "The Art of Computer Science", "9900333X");
-        writer.close();
-
-        // 2. query
-        String querystr = args.length > 0 ? args[0] : "lucene";
-
-        // the "title" arg specifies the default field to use
-        // when no field is explicitly specified in the query.
-        Query q = new QueryParser("title", analyzer).parse(querystr);
-
-        // 3. search
         int hitsPerPage = 10;
-        IndexReader reader = DirectoryReader.open(index);
+        IndexReader reader = DirectoryReader.open(getIndex());
         IndexSearcher searcher = new IndexSearcher(reader);
-        TopDocs docs = searcher.search(q, hitsPerPage);
+        TopDocs docs = searcher.search(query, hitsPerPage);
         ScoreDoc[] hits = docs.scoreDocs;
 
-        // 4. display results
         System.out.println("Found " + hits.length + " hits.");
         for(int i=0;i<hits.length;++i) {
             int docId = hits[i].doc;
+            System.out.println("Docid: " + docId );
             Document d = searcher.doc(docId);
             System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
         }
 
-        // reader can only be closed when there
-        // is no need to access the documents any more.
         reader.close();
     }
 	
-	private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+	private static StandardAnalyzer getAnalyzer() {
+		if ( analyzerInstance == null ) {
+			analyzerInstance = new StandardAnalyzer();
+		}
+		return analyzerInstance;
+	}	
+
+	private static Directory getIndex() {
+		if ( indexInstance == null ) {
+			indexInstance = new RAMDirectory();
+		}
+		return indexInstance;
+	}
+	
+	private static void addDocuments() throws IOException {
+		IndexWriterConfig config = new IndexWriterConfig(getAnalyzer());
+		IndexWriter writer = new IndexWriter(getIndex(), config);
+        addDocument(writer, "Lucene in Action", "193398817");
+        addDocument(writer, "Managing Gigabytes", "55063554A");
+        addDocument(writer, "The Art of Computer Science", "9900333X");
+        addDocument(writer, "Lucene for Dummies", "55320055Z");
+        writer.close();
+	}
+
+	private static void addDocument(IndexWriter w, String title, String isbn) throws IOException {
         Document doc = new Document();
         doc.add(new TextField("title", title, Field.Store.YES));
 
         // use a string field for isbn because we don't want it tokenized
         doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        
         w.addDocument(doc);
     }
 }
